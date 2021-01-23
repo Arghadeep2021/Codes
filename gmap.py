07460f541c9b0d04a3cff70a375130c8298da48e
Arghadeep2021 <77092816+Arghadeep2021@users.noreply.github.com>
2021-01-23 17:57:27 +0530

    Add files via upload

---
+++ gmap.py
@@ -0,0 +1,10 @@
+
+import gmplot
+
+gmap1 = gmplot.GoogleMapPlotter(30.3164945,
+                                78.03219179999999, 13)
+
+gmap1.apikey = "AIzaSyAnfzVLptjNumvs5PdLAG9rEGWJShxrhcs"
+
+gmap1.draw("C:\\Users\\user\\Desktop\\map11.html")
+
